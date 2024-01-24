ad3115f74f2edad632582ec2813665eb0f9acd26
Saif <dev.mdsaifulislam@gmail.com>
2024-01-24 13:29:55 +0600

    Operator overloading

---
+++ code.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    int a = 5;
+    printf("%d\n",a );
+    return 0;
+}
---
+++ operator_overloade.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self, name , age, height, weight) -> None:
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+class Cricketer(Person):
+    def __init__(self, name, age, height, weight, team) -> None:
+        self.team = team
+        super().__init__(name, age, height, weight)
+    def __add__(self, others):
+        return self.age + others.age
+
+s = Cricketer('shakib', 36, 5.6, 78, 'BAN')
+m = Cricketer('mushi',36, 5.5, 70, 'BAN')
+
+print(s+m)
